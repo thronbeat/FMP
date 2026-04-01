f251cd20440e80dc02dfbade7fb9cb50144d8999
HAKIZIMANA NATETE Ephrem <95871311+thronbeat@users.noreply.github.com>
2026-04-01 15:01:37 +0200

    Delete JS directory

--- JS/dashboard.js
+++
@@ -1,6 +0,0 @@
-        // Logout functionality
-        document.querySelector('.logout-btn').addEventListener('click', function() {
-            if(confirm('Are you sure you want to logout?')) {
-                alert('Logging out...');
-            }
-        });
